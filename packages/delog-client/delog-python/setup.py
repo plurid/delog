8003a82c7d8140b86ce567d226deb9b917ee5212
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-10-04 09:36:32 +0300

    delog-client/delog-python > setup: package

--- packages/delog-client/delog-python/setup.py
+++ packages/delog-client/delog-python/setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/plurid/delog",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=("tests",)),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
