c7cb03cca30d1e4cddd020c86dc8af3397b5a348
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-10-06 11:14:16 +0300

    delog-server > dev: shared/kernel > graphql

---
+++ packages/delog-server/source/shared/kernel/services/graphql/query/analytics/index.ts
@@ -0,0 +1,38 @@
+// #region imports
+    // #region libraries
+    import gql from 'graphql-tag';
+    // #endregion libraries
+// #endregion imports
+
+
+
+// #region module
+const GET_ANALYTICS_LAST_PERIOD = gql`
+    query GetAnalyticsLastPeriod {
+        getAnalyticsLastPeriod {
+            status
+            error {
+                path
+                type
+                message
+            }
+            data {
+                fatal
+                error
+                warn
+                info
+                debug
+                trace
+            }
+        }
+    }
+`;
+// #endregion module
+
+
+
+// #region exports
+export {
+    GET_ANALYTICS_LAST_PERIOD,
+};
+// #endregion exports
