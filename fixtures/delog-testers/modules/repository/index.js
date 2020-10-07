692c952dec93183f4bf4c3649417e13b66e33f2a
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-10-07 13:30:49 +0300

    fixtures/delog-testers > dev: repository

---
+++ fixtures/delog-testers/modules/repository/index.js
@@ -0,0 +1,28 @@
+const delog = require('@plurid/delog').default;
+
+const {
+    delogLevels,
+}= require('@plurid/delog');
+
+
+
+const main = () => {
+    const endpoint = 'http://localhost:56965/delog';
+    const token = '__TESTS__';
+
+    delog({
+        text: 'Delogged from main().',
+
+        endpoint,
+        token,
+
+        project: 'delog',
+        space: 'fixtures/delog-testers',
+
+        level: delogLevels.info,
+        method: 'repository',
+        format: '%LEVEL %TIME %TEXT',
+    });
+}
+
+main();
