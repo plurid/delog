df5aae89599614b6aa8ff71cef45c97e2bea9e58
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-10-08 14:52:42 +0300

    fixtures/delog-testers > dev: repository

---
+++ fixtures/delog-testers/modules/repository/subfolder/index.js
@@ -0,0 +1,41 @@
+// subfolder
+
+const delog = require('@plurid/delog').default;
+
+const {
+    delogLevels,
+}= require('@plurid/delog');
+
+
+
+const main = () => {
+    const endpoint = 'http://localhost:56965/delog';
+    const token = '__TESTS__';
+
+    delog({
+        text: 'Delogged from main().',
+
+        endpoint,
+        token,
+
+        project: 'delog',
+        space: 'fixtures/delog-testers',
+
+        level: delogLevels.info,
+        method: 'repository',
+        format: '%LEVEL %TIME %TEXT',
+
+        context: {
+            call: {
+                repository: {
+                    provider: 'github',
+                    name: 'plurid/delog',
+                    branch: 'master',
+                    commit: 'latest',
+                },
+            },
+        },
+    });
+}
+
+main();
