0d3d7ae12186c0f47af5181754edbf6c2591264b
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-10-11 13:57:56 +0300

    delog-client/delog-cli > dev: record

--- packages/delog-client/delog-cli/source/commands/record/index.ts
+++ packages/delog-client/delog-cli/source/commands/record/index.ts
@@ -1,4 +1,9 @@
 // #region imports
+    // #region libraries
+    import delog from '@plurid/delog';
+    // #endregion libraries
+
+
     // #region external
     import {
         RECORD,
@@ -20,49 +25,74 @@ const record = async (
 ) => {
     try {
         const {
-            delog,
+            delog: graphqlClient,
             configuration,
         } = await getDelog(
             server,
             identonym,
         );
 
-        if (!delog || !configuration) {
+        if (!graphqlClient || !configuration) {
             console.log('Could record to delog. Not logged in.');
             return;
         }
 
-        const input = {
+        const logged = await delog({
+            graphqlClient,
+
             text: data.text,
-            time: Math.floor(Date.now() / 1000),
-            level: data.level || 0,
+            level: data.level,
 
-            project: data.project || '',
-            space: data.space || '',
+            project: data.project,
+            space: data.space,
 
-            format: data.format || configuration.defaults.format || '%TIME %TEXT',
+            format: data.format || configuration.defaults.format,
 
-            method: data.method || '',
-            error: data.error || '',
-            extradata: data.extradata || '',
+            method: data.method,
+            error: data.error,
+            extradata: data.extradata,
             context: data.context || {},
-        };
-
-        const mutation = await delog.mutate({
-            mutation: RECORD,
-            variables: {
-                input,
-            },
         });
 
-        const response = mutation.data.delogMutationRecord;
-
-        if (!response.status) {
+        if (!logged) {
             console.log('Could not record to delog. Something went wrong.');
             return;
         }
 
         console.log(`Recorded to delog.`);
+
+
+        // const input = {
+        //     text: data.text,
+        //     time: Math.floor(Date.now() / 1000),
+        //     level: data.level || 0,
+
+        //     project: data.project || '',
+        //     space: data.space || '',
+
+        //     format: data.format || configuration.defaults.format || '%TIME %TEXT',
+
+        //     method: data.method || '',
+        //     error: data.error || '',
+        //     extradata: data.extradata || '',
+        //     context: data.context || {},
+        // };
+
+        // const mutation = await delog.mutate({
+        //     mutation: RECORD,
+        //     variables: {
+        //         input,
+        //     },
+        // });
+
+        // const response = mutation.data.delogMutationRecord;
+
+        // if (!response.status) {
+        //     console.log('Could not record to delog. Something went wrong.');
+        //     return;
+        // }
+
+        // console.log(`Recorded to delog.`);
 
         return;
     } catch (error) {
