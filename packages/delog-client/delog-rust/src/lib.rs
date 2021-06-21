a6d2a699ecede0309e833b9cf8993ac215dbe1ed
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-06-21 22:12:54 +0300

    delog-client/delog-rust > dev: source

--- packages/delog-client/delog-rust/src/lib.rs
+++ packages/delog-client/delog-rust/src/lib.rs
@@ -1,18 +1,19 @@
+#[derive(Default)]
 pub struct DelogData {
-    // Required.
+    /// Required.
     text: &'static str,
 
 
     // Configuration optionals.
-    // graphql_client?: ApolloClient<NormalizedCacheObject>,
+    // graphql_client: ApolloClient<NormalizedCacheObject>,
 
-    // endpoint?: &'static str,
-    // token?: &'static str,
+    endpoint: Option<&'static str>,
+    token: Option<&'static str>,
 
-    // project?: &'static str,
-    // space?: &'static str,
+    project: Option<&'static str>,
+    space: Option<&'static str>,
 
-    // format?: &'static str,
+    format: Option<&'static str>,
 
 
     // Logging optionals.
@@ -28,27 +29,27 @@ pub struct DelogData {
     // * + TRACE: 1;
     // * + ALL: 0.
     // */
-    // level?: number;
+    // level: i8,
 
     // /**
     // * To be used if the `delog` is meant to be fired only in 'TESTING' `mode` (`context.mode`),
     // * and the `mode` is set dinamically/from outside the enclosing function.
     // */
-    // tester?: boolean;
+    // tester: bool,
 
     // /**
     // * Name of the method from where the log originates.
     // */
-    // method?: &'static str,
+    method: Option<&'static str>,
 
-    // error?: Error;
+    // error: Error,
 
     // /**
     // * Arbitrary data: a simple string, stringified JSON or deon.
     // */
-    // extradata?: &'static str,
+    extradata: Option<&'static str>,
 
-    // context?: DelogContext;
+    // context: DelogContext,
 }
 
 
@@ -59,26 +60,79 @@ pub enum DelogCall {
 
 
 fn delog_call (
-    text: &'static str,
+    text: String,
+    endpoint: String,
+    token: String,
+    project: String,
+    space: String,
+    format: String,
+    method: String,
+    extradata: String,
 ) {
-    println!("{}", text);
+    println!(
+        "{} {} {} {} {} {} {} {}", 
+        text, 
+        endpoint, 
+        token,
+        project,
+        space,
+        format,
+        method,
+        extradata,
+    );
 }
 
 
 pub fn delog(
     data: DelogCall,
 ) -> bool {
+    use std::env;
     use DelogCall::*;
+
+    let endpoint: String = env::var("DELOG_ENDPOINT").unwrap_or("".to_string());
+    let token: String = env::var("DELOG_TOKEN").unwrap_or("".to_string());
+
+    let project: String = env::var("DELOG_PROJECT").unwrap_or("".to_string());
+    let space: String = env::var("DELOG_SPACE").unwrap_or("".to_string());
+    let format: String = env::var("DELOG_FORMAT").unwrap_or("".to_string());
+  
+    let method: String = env::var("DELOG_METHOD").unwrap_or("".to_string());
+    let extradata: String = env::var("DELOG_EXTRADATA").unwrap_or("".to_string());
+
 
     match data {
         Str(data) => {
             delog_call(
-                data,
+                String::from(data),
+                endpoint,
+                token,
+                project,
+                space,
+                format,
+                method,
+                extradata,
             );
         }
         Data(data) => {
+            let data_endpoint = String::from(data.endpoint.unwrap_or(&endpoint));
+            let data_token = String::from(data.token.unwrap_or(&token));
+
+            let data_project = String::from(data.project.unwrap_or(&project));
+            let data_space = String::from(data.space.unwrap_or(&space));
+            let data_format = String::from(data.format.unwrap_or(&format));
+          
+            let data_method = String::from(data.method.unwrap_or(&method));
+            let data_extradata = String::from(data.extradata.unwrap_or(&extradata));
+
             delog_call(
-                data.text,
+                String::from(data.text),
+                data_endpoint,
+                data_token,
+                data_project,
+                data_space,
+                data_format,
+                data_method,
+                data_extradata,
             );
         }
     }
@@ -104,6 +158,7 @@ mod tests {
     fn it_works_data() {
         let delog_data = DelogData {
             text: "it works with data",
+            ..Default::default()
         };
         let sent = delog(DelogCall::Data(delog_data));
         assert_eq!(sent, true);
