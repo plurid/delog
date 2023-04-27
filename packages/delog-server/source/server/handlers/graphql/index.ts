5ee39d687d4d24012ab20baf58cca990545c7fe9
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2023-04-27 13:21:07 +0300

    delog-server > dev: server/handlers/graphql

--- packages/delog-server/source/server/handlers/graphql/index.ts
+++ packages/delog-server/source/server/handlers/graphql/index.ts
@@ -1,18 +1,23 @@
 // #region imports
     // #region libraries
+    import http from 'node:http';
+
     import {
         Application,
     } from 'express';
 
     import {
         ApolloServer,
-    } from 'apollo-server-express';
+    } from '@apollo/server';
+
+    import { expressMiddleware } from '@apollo/server/express4';
+
+    import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
 
     import {
-        ApolloServerPluginLandingPageDisabled,
-        ApolloServerPluginLandingPageGraphQLPlayground,
-        Config,
-    } from 'apollo-server-core';
+        ApolloServerPluginLandingPageLocalDefault,
+        ApolloServerPluginLandingPageProductionDefault,
+    } from '@apollo/server/plugin/landingPage/default';
     // #endregion libraries
 
 
@@ -70,72 +75,78 @@ const setupGraphQLServer = async (
         ? logic.logger
         : defaultLogger;
 
+    const httpServer = http.createServer(instance);
+
     const graphQLServer = new ApolloServer({
-        typeDefs: schemas as Config['typeDefs'],
+        typeDefs: schemas,
         resolvers,
-        context: async ({
-            req,
-            res,
-        }: any) => {
-            const privateOwnerIdentonym = privateUsage
-                ? await getPrivateOwner(req)
-                : '';
-            logger.log(`[Delog Debug : Context] :: ${privateOwnerIdentonym}`, logLevels.debug);
-
-            const {
-                tokens,
-                projects,
-                spaces,
-                providers,
-                repositories,
-                formats,
-                notifiers,
-                testers,
-            } = await loadData(
-                privateOwnerIdentonym,
-            );
-
-            const context: Context = {
-                request: req,
-                response: res,
-
-                instance,
-
-                tokens,
-                projects,
-                spaces,
-                providers,
-                repositories,
-                formats,
-                notifiers,
-                testers,
-
-                customLogicUsage,
-
-                privateUsage,
-                privateOwnerIdentonym,
-
-                logger,
-                logLevel,
-                logLevels,
-            };
-
-            return context;
-        },
         plugins: [
+            ApolloServerPluginDrainHttpServer({ httpServer }),
             environment.production
-                ? ApolloServerPluginLandingPageDisabled()
-                : ApolloServerPluginLandingPageGraphQLPlayground({
-                    ...playground,
-                }),
+                ? ApolloServerPluginLandingPageProductionDefault({})
+                // ? {}
+                : ApolloServerPluginLandingPageLocalDefault({}),
         ],
     });
+
+    const context = async ({
+        req,
+        res,
+    }: any) => {
+        const privateOwnerIdentonym = privateUsage
+            ? await getPrivateOwner(req)
+            : '';
+        logger.log(`[Delog Debug : Context] :: ${privateOwnerIdentonym}`, logLevels.debug);
+
+        const {
+            tokens,
+            projects,
+            spaces,
+            providers,
+            repositories,
+            formats,
+            notifiers,
+            testers,
+        } = await loadData(
+            privateOwnerIdentonym,
+        );
+
+        const context: Context = {
+            request: req,
+            response: res,
+
+            instance,
+
+            tokens,
+            projects,
+            spaces,
+            providers,
+            repositories,
+            formats,
+            notifiers,
+            testers,
+
+            customLogicUsage,
+
+            privateUsage,
+            privateOwnerIdentonym,
+
+            logger,
+            logLevel,
+            logLevels,
+        };
+
+        return context;
+    }
+
     await graphQLServer.start();
 
-    graphQLServer.applyMiddleware({
-        app: instance,
-        path: GRAPHQL_ENDPOINT,
-    });
+    instance.use(
+        GRAPHQL_ENDPOINT,
+        expressMiddleware(graphQLServer, {
+            context,
+        }),
+    );
 }
 // #endregion module
 
